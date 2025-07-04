25a96b29b3781fdb82cbbf31adf75fd8c33a7d52
William Ordoñez <williamsony@hotmail.com>
2025-07-04 09:21:02 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+      <h1>Curso Express.js V4</h1>
+      <p>Esto es una aplicación node.js con express.js</p>
+      <p>Corre en el puerto: ${PORT}</p>
+    `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
