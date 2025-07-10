b86b371c065cb7ecb28bb88043c6629a05c8efee
wordonez <williamsony@hotmail.com>
2025-07-10 09:32:45 -0500

    Ingreso de dotenv

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+console.log(`Server is running on port ${PORT}`);
 app.get('/', (req, res) => {
   res.send(`
       <h1>Curso Express.js V4 rama Developer..</h1>
