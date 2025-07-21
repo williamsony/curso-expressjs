b2fb45db8eeec5b9bdd0c5c8cd42bc8c6fdfa259
wordonez <williamsony@hotmail.com>
2025-07-21 11:12:46 -0500

    Cambios menores

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 console.log(`Server is running on port ${PORT}`);
 app.get('/', (req, res) => {
   res.send(`
-      <h1>Curso Express.js V4 rama Developer..</h1>
+      <h1>Curso Express.js V4 rama Developer.. </h1>
       <p>Esto es una aplicación node.js con express.js Test desde developer</p>
       <p>Corre en el puerto: ${PORT}</p>
     `);
