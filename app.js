ee2f8c2baf3c5ebaacac147c0d9abf88b718ca07
wordonez <williamsony@hotmail.com>
2025-08-06 13:57:56 -0500

    2025-08

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log(`Server is running on port ${PORT}`);
 app.get('/', (req, res) => {
   res.send(`
       <h1>Curso Express.js V4 rama Developer.. </h1>
-      <p>Esto es una aplicación node.js con express.js Test desde developer</p>
+      <p>Esto es una aplicación node.js con express.js Test desde developer.</p>
       <p>Corre en el puerto: ${PORT}</p>
     `);
 });
